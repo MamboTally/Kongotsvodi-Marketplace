06ad8c196ab048148c9a450dffd9910c4f0ad1ed
Talent Charambanyi <tkeldons@gmail.com>
2022-11-25 08:38:06 +0200

    first commit

---
+++ RTW3-Week7-NFT-Marketplace/hardhat.config.js
@@ -0,0 +1,35 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+const fs = require('fs');
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+
+// eslint-disable-next-line no-undef
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    goerli: {
+      url: "https://eth-goerli.g.alchemy.com/v2/afo3abh7NnO4hucxKxlnlFT9jav_iHsQ",
+      accounts:  "39ff22676135f32df5aea68587fbc701cee3ae03c8feb11fee57b0ff5b6b73b7" 
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
---
+++ RTW3-Week7-NFT-Marketplace/src/pinata.js
@@ -0,0 +1,93 @@
+//require('dotenv').config();
+// eslint-disable-next-line no-unused-vars
+const key = process.env.REACT_APP_PINATA_KEY;
+// eslint-disable-next-line no-unused-vars
+const secret = process.env.REACT_APP_PINATA_SECRET;
+
+const axios = require('axios');
+const FormData = require('form-data');
+
+export const uploadJSONToIPFS = async(JSONBody) => {
+    const url = `https://api.pinata.cloud/pinning/pinJSONToIPFS`;
+    //making axios POST request to Pinata ⬇️
+    return axios 
+        .post(url, JSONBody, {
+            headers: {
+                pinata_api_key: "cd77337a7988c9b96662",
+                pinata_secret_api_key: "fde5173e5b3c4cfeb79ccac7569836b7ee605fc98a3a12e9df330c1ef3510c69",
+            }
+        })
+        .then(function (response) {
+           return {
+               success: true,
+               pinataURL: "https://gateway.pinata.cloud/ipfs/" + response.data.IpfsHash
+           };
+        })
+        .catch(function (error) {
+            console.log(error)
+            return {
+                success: false,
+                message: error.message,
+            }
+
+    });
+};
+
+export const uploadFileToIPFS = async(file) => {
+    const url = `https://api.pinata.cloud/pinning/pinFileToIPFS`;
+    //making axios POST request to Pinata ⬇️
+    
+    let data = new FormData();
+    data.append('file', file);
+
+    const metadata = JSON.stringify({
+        name: 'testname',
+        keyvalues: {
+            exampleKey: 'exampleValue'
+        }
+    });
+    data.append('pinataMetadata', metadata);
+
+    //pinataOptions are optional
+    const pinataOptions = JSON.stringify({
+        cidVersion: 0,
+        customPinPolicy: {
+            regions: [
+                {
+                    id: 'FRA1',
+                    desiredReplicationCount: 1
+                },
+                {
+                    id: 'NYC1',
+                    desiredReplicationCount: 2
+                }
+            ]
+        }
+    });
+    data.append('pinataOptions', pinataOptions);
+
+    return axios 
+        .post(url, data, {
+            maxBodyLength: 'Infinity',
+            headers: {
+                'Content-Type': `multipart/form-data; boundary=${data._boundary}`,
+                pinata_api_key: "cd77337a7988c9b96662",
+                pinata_secret_api_key: "fde5173e5b3c4cfeb79ccac7569836b7ee605fc98a3a12e9df330c1ef3510c69",
+            }
+        })
+        .then(function (response) {
+            console.log("image uploaded", response.data.IpfsHash)
+            return {
+               success: true,
+               pinataURL: "https://gateway.pinata.cloud/ipfs/" + response.data.IpfsHash
+           };
+        })
+        .catch(function (error) {
+            console.log(error)
+            return {
+                success: false,
+                message: error.message,
+            }
+
+    });
+};
